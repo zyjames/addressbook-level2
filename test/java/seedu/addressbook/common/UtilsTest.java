e23793b6a9f9063876e995f518a37f45f89f544a
Zhiyuan <jameslaizy@gmail.com>
2017-02-08 05:30:55 +0800

    Revert "Add new JUnit test and modified the test cases’ names to go according to naming standards."
    This reverts commit 035789789923b93f1550f2d2b81d7b4caecd56b3.

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -3,7 +3,6 @@ package seedu.addressbook.common;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,111 +10,56 @@ import org.junit.Test;
 
 public class UtilsTest {
     @Test
-    public void emptyListShouldReturnFalse() {
+    public void isAnyNull() {
         // empty list
-        assertIsNotNull();
-    }
-    
-    @Test
-    public void anyEmptyListShouldReturnFalse() {
+        assertFalse(Utils.isAnyNull());
+
         // Any non-empty list
-        assertIsNotNull(new Object(), new Object());
-        assertIsNotNull("test");
-        assertIsNotNull("");
-    }
-    
-    @Test
-    public void nonEmptyListWithStartElementNullShouldReturnTrue() {
+        assertFalse(Utils.isAnyNull(new Object(), new Object()));
+        assertFalse(Utils.isAnyNull("test"));
+        assertFalse(Utils.isAnyNull(""));
+
         // non empty list with just one null at the beginning
-        assertIsAnyNull((Object) null);
-        assertIsAnyNull(null, "", new Object());
-        assertIsAnyNull(null, new Object(), new Object());
-    }
-    
-    @Test
-    public void nonEmptyListWithMiddleElementNullShouldReturnTrue() {
+        assertTrue(Utils.isAnyNull((Object) null));
+        assertTrue(Utils.isAnyNull(null, "", new Object()));
+        assertTrue(Utils.isAnyNull(null, new Object(), new Object()));
+
         // non empty list with nulls in the middle
-        assertIsAnyNull(new Object(), null, null, "test");
-        assertIsAnyNull("", null, new Object());
-    }
-    
-    @Test
-    public void nonEmptyListWithLastElementNullShouldReturnTrue() {
+        assertTrue(Utils.isAnyNull(new Object(), null, null, "test"));
+        assertTrue(Utils.isAnyNull("", null, new Object()));
+
         // non empty list with one null as the last element
-        assertIsAnyNull("", new Object(), null);
-        assertIsAnyNull(new Object(), new Object(), null);
-    }
-    
-    @Test
-    public void nonEmptyListWithElementNotNullShouldReturnTrue() {
-        // non empty list with one null as the last element
-        assertIsNotNull("test");
-        assertIsNotNull(true);
-        assertIsNotNull(false);
-        assertIsNotNull('a');
-        assertIsNotNull(1);
-    }
-    
-    @Test
-    public void nonEmptyListWithNullElementShouldReturnFalse() {
+        assertTrue(Utils.isAnyNull("", new Object(), null));
+        assertTrue(Utils.isAnyNull(new Object(), new Object(), null));
+
         // confirms nulls inside the list are not considered
         List<Object> nullList = Arrays.asList((Object) null);
-        assertIsNotNull(nullList);
+        assertFalse(Utils.isAnyNull(nullList));
     }
-    
+
     @Test
-    public void nonEmptyNullListShouldReturnFalse() {
-        // non empty list of null list
-        ArrayList<List<Object>> nullListList = new ArrayList<List<Object>>();
-        nullListList.add(Arrays.asList((Object) null));
-        assertIsNotNull(nullListList);
-    }
-    
-    @Test
-    public void emptyListElementsAreUniqueShouldReturnTrue() throws Exception {
+    public void elementsAreUnique() throws Exception {
         // empty list
         assertAreUnique();
-    }
-    
-    @Test
-    public void listWithOneElementAreUniqueShouldReturnTrue() throws Exception {
+
         // only one object
         assertAreUnique((Object) null);
         assertAreUnique(1);
         assertAreUnique("");
         assertAreUnique("abc");
-        assertAreUnique("a", 'a');
-        assertAreUnique(1, 1.0);
-        assertAreUnique("123", 123);
-    }
-    
-    @Test
-    public void listWithAllUniqueElementsShouldReturnTrue() throws Exception {
+
         // all objects unique
         assertAreUnique("abc", "ab", "a");
         assertAreUnique(1, 2);
-    }
-    
-    @Test
-    public void listWithDuplicateElementsShouldReturnFalse() throws Exception {
+
         // some identical objects
-        assertNotUnique("", "");
         assertNotUnique("abc", "abc");
         assertNotUnique("abc", "", "abc", "ABC");
         assertNotUnique("", "abc", "a", "abc");
         assertNotUnique(1, new Integer(1));
-        assertNotUnique(new Integer(1), null, new Integer(1));
-        assertNotUnique(null, 123, new Integer(123));
+        assertNotUnique(null, 1, new Integer(1));
         assertNotUnique(null, null);
         assertNotUnique(null, "a", "b", null);
-    }
-    
-    private void assertIsNotNull(Object... objects) {
-        assertFalse(Utils.isAnyNull(objects));
-    }
-    
-    private void assertIsAnyNull(Object...objects) {
-        assertTrue(Utils.isAnyNull(objects));
     }
 
     private void assertAreUnique(Object... objects) {
