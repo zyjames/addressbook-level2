c5837146de3003ae5df8107423adf61893b0385b
Zhiyuan <jameslaizy@gmail.com>
2017-02-08 06:44:20 +0800

    Add fix to similar but all small character name

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -64,7 +64,7 @@ public class Name {
      * Returns true if the otherName is similar to this name
      */
     public boolean isSimilar(Name otherName) {
-        if (otherName instanceof Name && this.fullName.equals(otherName.fullName)) {
+        if (otherName instanceof Name && this.fullName.equalsIgnoreCase(otherName.fullName.toLowerCase())) {
             return true;
         } else {
             return false;
